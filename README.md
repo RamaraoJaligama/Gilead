4982e51ea41a7ef95f09d6a5220f4ec05cde615f
Ramarao <ramarao.jaligama@popcornapps.com>
2014-01-30 14:30:59 +0530

    Gilead new setup
    gilead new setup

--- README.md
+++
@@ -1,4 +0,0 @@
-Gilead
-======
-
-Gilead Touch base Application
---
+++ js/common.js
@@ -0,0 +1,35 @@
+function advancedcolleaguesearch(){
+	$.mobile.changePage( "advancedcolleaguesearch.html", { transition: "slide", changeHash: false });
+}
+
+function advancedroomsearch(){
+	$.mobile.changePage( "advancedroomsearch.html", { transition: "slide", changeHash: false });
+}
+
+function corporatenumbers(){
+	$.mobile.changePage( "corporatenumbers.html", { transition: "slide", changeHash: false });
+}
+
+function campusMaps(){
+	$.mobile.changePage( "campus_maps.html", { transition: "slide", changeHash: false });
+}
+
+function me(){
+	$.mobile.changePage( "me.html", { transition: "slide", changeHash: false });
+}
+
+function searchAll(){
+	
+	$.mobile.changePage( "searchresults-all.html", { transition: "slide", changeHash: false });
+	
+}
+
+function colleaguedetails(){
+	$.mobile.changePage( "colleguedetails.html", { transition: "slide", changeHash: false });
+}
+function mepage(){
+	$.mobile.changePage( "me.html", { transition: "slide", changeHash: false });
+}
+function roomdetails(){
+	$.mobile.changePage( "roomdetails.html", { transition: "slide", changeHash: false });
+}
